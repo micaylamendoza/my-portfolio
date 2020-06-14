1af18bc408c6a6904e0fd7403659d367ca7106a9
micaylamendoza <micaylamendoza@gmail.com>
2020-06-14 14:52:52 -0700

    use fetch() to request content from DataServlet.java and adds it to the page when user pushes button to get message

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -24,3 +24,8 @@ function closeNav() {
 }
 
 
+function getMessage() {
+  fetch('/data').then(response => response.text()).then((message) => {
+    document.getElementById('message-container').innerText = message;
+  });
+}
